9d1706132aec83b3352fd34146f26684203b63e0
Mujhtech <mujeeb.muhideen@gmail.com>
2021-06-23 10:42:59 +0100

    ...

---
+++ config/nav-toastr.php
@@ -0,0 +1,18 @@
+<?php
+
+
+/*
+ * This file is part of the Laravel Navigation Toastr package.
+ *
+ * (c) Mujtech Mujeeb <mujeeb.muhideen@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+return [
+
+
+    
+];
